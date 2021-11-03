116bf870fd5bb56bf274c935f0b62d93aee72a80
Pengyuan Huang <pyhuang@umich.edu>
2021-11-02 20:40:30 -0400

    update servelss-api

---
+++ serverless-api/src/handlers/post.ts
@@ -0,0 +1,16 @@
+import Store from '../posts_store'
+
+const Post = async (request: any) => {
+  // This will be used soon to retrieve a post
+  const posts = new Store()
+  const postId = request.params.id
+
+  const body = JSON.stringify(await posts.find(postId))
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-type': 'application/json'
+  }
+  return new Response(body, { headers })
+}
+
+export default Post
---
+++ serverless-api/src/handlers/posts.ts
@@ -0,0 +1,15 @@
+import Store from '../posts_store'
+
+// const posts = []
+
+const Posts = async () => {
+  const posts = new Store()
+  const body = JSON.stringify(await posts.all())
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-type': 'application/json'
+  }
+  return new Response(body, { headers })
+}
+
+export default Posts
